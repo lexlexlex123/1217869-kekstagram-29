49272d1669d45a12ba4ef9ff1f188db9e0613c5d
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-06-12 07:59:56 +0300

    функции

--- js/functions.js
+++ js/functions.js
@@ -5,16 +5,16 @@ function correctLength(string, length) {
 function polindrom(string) {
   string = string.replaceAll(' ','');
   let reverseString = '';
-  for (let i=string.length-1; i >= 0; i--) {
+  for (let i = string.length - 1; i >= 0; i--) {
     reverseString += string[i];
   }
-  return reverseString == string;
+  return reverseString === string;
 }
 
 function digitInString(string) {
   string = string.toString();
   let newString = '';
-  for (let i=0; i < string.length; i++) {
+  for (let i = 0; i < string.length; i++) {
     if (!isNaN(string[i])) {
       newString += string[i];
     }
