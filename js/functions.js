0b0b97bcd8575e1bf07cfdce185a2ef424052460
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-06-13 06:08:33 +0300

    функции

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ function correctLength(string, length) {
 }
 
 function polindrom(string) {
-  string = string.replaceAll(' ','');
+  string = string.replaceAll(' ','').toLowerCase();
   let reverseString = '';
   for (let i = string.length - 1; i >= 0; i--) {
     reverseString += string[i];
@@ -12,7 +12,7 @@ function polindrom(string) {
 }
 
 function digitInString(string) {
-  string = string.toString();
+  string = string.toString().replaceAll(' ','');
   let newString = '';
   for (let i = 0; i < string.length; i++) {
     if (!isNaN(string[i])) {
@@ -21,3 +21,16 @@ function digitInString(string) {
   }
   return newString ? newString : NaN;
 }
+
+correctLength('проверяемая строка', 20);
+correctLength('проверяемая строка', 18);
+correctLength('проверяемая строка', 10);
+polindrom('топот');
+polindrom('Довод');
+polindrom('Кекс');
+polindrom('Лёша на полке клопа нашёл ');
+digitInString('2023 год');
+digitInString('ECMAScript 2022');
+digitInString('1 кефир,0.5 батона');
+digitInString('агент 007');
+digitInString('а я томат');
