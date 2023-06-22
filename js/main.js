7c10688b5a36ad5ca50927ec8941f82cdd825475
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-06-22 19:33:59 +0300

    добавлены все модули

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-import './functions.js';
+import {getDescPhoto} from './describe-photo.js';
+
+//массив со всеми объектами
+const describePhotos = (count) => {
+  return Array(count).fill(null).map((_, index) => getDescPhoto(index));
+}
+
+describePhotos(25);
