529765ac3a2b1c9a07f2cf2d000b07640aa0962b
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-07-08 06:01:38 +0300

    разбивка на функции

--- js/full-window.js
+++ js/full-window.js
@@ -1,11 +1,65 @@
 import {allPhotos} from './picture.js';
 
+const countAdd = (element) => {
+  const countListComment = Number(document.querySelector('.comments-count').textContent);
+  const count = element.children.length;
+
+  if (count + 5 < countListComment) {
+    return count + 5;
+  }
+  document.querySelector('.comments-loader').classList.add('hidden');
+  return countListComment;
+};
+
+const renderComment = (picture) => {
+  const listComment = allPhotos.filter(({id}) => String(id) === picture.querySelector('.picture__img').id)[0].comments;
+  const countListComment = Number(document.querySelector('.comments-count').textContent);
+  const socialComment = document.querySelector('.social__comments');
+  const count = countAdd(socialComment);
+  socialComment.innerHTML = '';
+
+  //заголовок перед комментами
+  const countTitle = document.querySelector('.social__comment-count');
+  countTitle.innerHTML = `
+    ${count} из <span class="comments-count">${countListComment}</span> комментариев
+  `;
+
+  for (let i = 0; i < count; i++) {
+    const comment = listComment[i];
+    const li = document.createElement('li');
+    li.classList.add('social__comment');
+    li.innerHTML = `
+    <img
+        class="social__picture"
+        src=${comment.avatar}
+        alt=${comment.name}
+        width="35" height="35">
+    <p class="social__text">${comment.message}</p>
+    `;
+    socialComment.appendChild(li);
+  }
+};
+
+const close = () => {
+  const bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.querySelector('.comments-loader').classList.remove('hidden');
+};
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    close();
+  }
+});
+
 window.addEventListener('load', () => {
   const pictures = document.querySelectorAll('.picture');
   const bigPicture = document.querySelector('.big-picture');
   const bigPictureClose = document.querySelector('.big-picture__cancel');
-
   pictures.forEach((picture) => picture.addEventListener('click', () => {
+    bigPicture.classList.remove('hidden');
     const bigPictureImg = document.querySelector('.big-picture__img img');
     bigPictureImg.src = picture.querySelector('.picture__img').src;
     const likeCount = document.querySelector('.likes-count');
@@ -14,76 +68,19 @@ window.addEventListener('load', () => {
     commentCount.textContent = picture.querySelector('.picture__comments').textContent;
     const description = document.querySelector('.social__caption');
     description.textContent = picture.querySelector('.picture__img').alt;
+    const socialComment = document.querySelector('.social__comments');
+    socialComment.innerHTML = '';
 
-    //комментарии пользователей
-    let count = 0;
-    const countAdd = () => {
-      const countListComment = Number(document.querySelector('.comments-count').textContent);
-
-      if (count + 5 < countListComment) {
-        return count + 5;
-      } else {
-        document.querySelector('.comments-loader').classList.add('hidden');
-        return countListComment;
-      }
-    };
-
-    const renderComment = () => {
-      const listComment = allPhotos.filter(({id}) => String(id) === picture.querySelector('.picture__img').id)[0].comments;
-      const countListComment = Number(document.querySelector('.comments-count').textContent);
-      const socialComment = document.querySelector('.social__comments');
-      socialComment.innerHTML = '';
-
-      count = countAdd();
-
-      //заголовок перед комментами
-      const countTitle = document.querySelector('.social__comment-count');
-      countTitle.innerHTML = `
-        ${count} из <span class="comments-count">${countListComment}</span> комментариев
-      `;
-
-      for (let i = 0; i < count; i++) {
-        const comment = listComment[i];
-        const li = document.createElement('li');
-        li.classList.add('social__comment');
-        li.innerHTML = `
-        <img
-            class="social__picture"
-            src=${comment.avatar}
-            alt=${comment.name}
-            width="35" height="35">
-        <p class="social__text">${comment.message}</p>
-        `;
-        socialComment.appendChild(li);
-      }
-    };
-
-    renderComment();
+    renderComment(picture);
     const loader = document.querySelector('.comments-loader');
     loader.addEventListener('click', () => {
-      renderComment();
+      renderComment(picture);
     });
 
-    bigPicture.classList.remove('hidden');
-
     //закрываем окно счетчика и прокрутку окна body
-    // document.querySelector('.social__comment-count').classList.add('hidden');
-    // document.querySelector('.comments-loader').classList.add('hidden');
     document.body.classList.add('modal-open');
   }));
-
-  const close = () => {
-    bigPicture.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    document.querySelector('.comments-loader').classList.remove('hidden');
-  };
-
   bigPictureClose.addEventListener('click', () => close());
-
-  document.addEventListener('keydown', (evt) => {
-    if (evt.key === 'Escape') {
-      evt.preventDefault();
-      close();
-    }
-  });
 });
+
+
