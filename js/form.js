713685130a1d252922c416ea88debe5ce0dcdb31
alex <lexpochta.86@gmail.com>
2023-07-11 06:18:26 +0300

    вызов настроек после выбора файла

--- js/form.js
+++ js/form.js
@@ -6,11 +6,33 @@ const addFormAttr = () => {
   form.enctype = 'multipart/form-data';
 };
 
+
 const loadFormImg = () => {
   const file = document.querySelector('#upload-file');
 
   file.addEventListener('change', () => {
-    console.log(file.value);
+    //console.log(file.files[0].name);
+    //отобразим картинку
+    const changeImg = document.querySelector('.img-upload__overlay');
+    changeImg.classList.remove('hidden');
+    //const image = document.querySelector('.img-upload__preview img');
+    //image.src = file.files[0].name;
+
+    //скрытие отображение картинки по клику на крестик
+    const closeImg = document.querySelector('#upload-cancel');
+    closeImg.addEventListener('click', () => {
+      changeImg.classList.add('hidden');
+    });
+
+    //скрытие отображения картинки по Esc
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        changeImg.classList.add('hidden');
+      }
+    });
+
+    //сбросить выбор файла в input
     file.value = '';
   });
 };
