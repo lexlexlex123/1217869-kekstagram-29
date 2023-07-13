9b85ac33e08dd6fa7b6733d85bff7c8f505bfb19
alex <lexpochta.86@gmail.com>
2023-07-13 06:47:36 +0300

    вывод сообщений на валидацию

--- js/form.js
+++ js/form.js
@@ -1,3 +1,39 @@
+//скрытие отображение картинки
+const close = () => {
+  const changeImg = document.querySelector('.img-upload__overlay');
+  const file = document.querySelector('#upload-file');
+  if (!changeImg.classList.contains('hidden')) {
+    changeImg.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    file.value = '';
+  }
+};
+
+//скрытие отображение картинки по клику на пустую область
+const closeImg = document.querySelector('.img-upload__overlay');
+closeImg.addEventListener('click', (evt) => {
+  if (evt.target === closeImg) {
+    close();
+  }
+});
+
+//скрытие отображение картинки по клику на крестик
+const closeButton = document.querySelector('#upload-cancel');
+closeButton.addEventListener('click', () => {
+  close();
+});
+
+//скрытие отображения картинки по Esc
+document.addEventListener('keydown', (evt) => {
+  const form = document.querySelector('.img-upload__form');
+  const noFocus = (form.querySelector('.text__hashtags') !== document.activeElement) && (form.querySelector('.text__description') !== document.activeElement);
+
+  if ((evt.key === 'Escape') && (noFocus)) {
+    evt.preventDefault();
+    close();
+  }
+});
+
 //добавляем аттрибуты форме
 const addFormAttr = () => {
   const form = document.querySelector('.img-upload__form');
@@ -6,37 +42,44 @@ const addFormAttr = () => {
   form.enctype = 'multipart/form-data';
 
   const pristine = new Pristine(form, {
-    classTo: 'form__item',
-    errorClass: 'form__item--invalid',
-    successClass: 'form__item--valid',
-    errorTextParent: 'form__item',
-    errorTextTag: 'span',
-    errorTextClass: 'form__error'
+    classTo: 'img-upload__text',
+    errorTextParent: 'img-upload__text',
+    errorTextTag: 'div',
+    errorTextClass: 'error-validate'
   });
 
   const validateHashTag = (value) => /^#[a-zа-яё0-9]{2,20}$/i.test(value);
 
-  const validateHashTags = () => {
+  const validateHashTagBlank = () => {
     const input = form.querySelector('.text__hashtags').value;
     const words = input.split(' ').filter((value) => value !== '').map((value) => value.toLowerCase());
-    //пустой хэштег
-    if (words.length === 0) {
-      return true;
-    }
-    //в одном из хэштегов ошибка
+    //недопустимый хэштег
     const countErrorHashTag = words.filter((value) => !validateHashTag(value));
     if (countErrorHashTag > 0) {
       return false;
     }
+    return true;
+  };
+
+  const validateHashTagCount = () => {
+    const input = form.querySelector('.text__hashtags').value;
+    const words = input.split(' ').filter((value) => value !== '').map((value) => value.toLowerCase());
     //количество хэштегов больше 5
     if (words.length > 5) {
       return false;
     }
+    return true;
+  };
+
+  const validateHashTagRepeat = () => {
+    const input = form.querySelector('.text__hashtags').value;
+    const words = input.split(' ').filter((value) => value !== '').map((value) => value.toLowerCase());
     //повторяющиеся хэштеги
     const repeatWords = new Set(words);
     if (words.length === repeatWords.size) {
       return true;
     }
+    return false;
   };
 
   const validateComments = () => {
@@ -47,13 +90,14 @@ const addFormAttr = () => {
     return true;
   };
 
-  pristine.addValidator(form.querySelector('.text__hashtags'), validateHashTags);
-  pristine.addValidator(form.querySelector('.text__description'), validateComments);
+  pristine.addValidator(form.querySelector('.text__hashtags'), validateHashTagBlank, 'недопустипый хэштег');
+  pristine.addValidator(form.querySelector('.text__hashtags'), validateHashTagCount, 'хэштегов больше 5');
+  pristine.addValidator(form.querySelector('.text__hashtags'), validateHashTagRepeat, 'есть повторяющиеся хэштеги');
+  pristine.addValidator(form.querySelector('.text__description'), validateComments, 'комментариев больше 140');
 
   form.addEventListener('submit', (evt) => {
     if (pristine.validate()) {
       evt.preventDefault();
-      const changeImg = document.querySelector('.img-upload__overlay');
       close();
     } else {
       evt.preventDefault();
@@ -76,44 +120,8 @@ const loadFormImg = () => {
     effectImgs.forEach((img) => {
       img.style.backgroundImage = `url(${image.src})`;
     });
-
-    //скрытие отображение картинки по клику на крестик
-    const closeImg = document.querySelector('#upload-cancel');
-    closeImg.addEventListener('click', () => {
-      close();
-    });
-
-    //скрытие отображения картинки по Esc
-    const form = document.querySelector('.img-upload__form');
-    document.addEventListener('keydown', (evt) => {
-      const noFocus = form.querySelector('.text__hashtags') !== document.activeElement;
-
-      if ((evt.key === 'Escape') && (noFocus)) {
-        evt.preventDefault();
-        close();
-      }
-    });
   });
 };
-
-//скрытие отображение картинки
-const close = () => {
-  const changeImg = document.querySelector('.img-upload__overlay');
-  const file = document.querySelector('#upload-file');
-  if (!changeImg.classList.contains('hidden')) {
-    changeImg.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-    file.value = '';
-  }
-};
-
-//скрытие отображение картинки по клику на пустую область
-const closeImg = document.querySelector('.img-upload__overlay');
-closeImg.addEventListener('click', (evt) => {
-  if (evt.target === closeImg) {
-    close();
-  };
-});
 
 addFormAttr();
 loadFormImg();
