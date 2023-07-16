c294887d84fee166b37f85f197500bad7ac02694
lexlexlex123 <44517871+lexlexlex123@users.noreply.github.com>
2023-07-16 22:06:28 +0300

    get image from server

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-import {describePhotos} from './describe-photo.js';
 import {createPhoto} from './picture.js';
 import {showFullWindowImg} from './full-window.js';
 import {validateForm, loadFormImg, scaleImage, changeFilterEffect} from './form.js';
 
-const allPhotos = describePhotos(25);
+const response = await fetch('https://29.javascript.pages.academy/kekstagram/data', {method: 'GET'});
+const getData = await response.json();
+
+const allPhotos = getData;
 createPhoto(allPhotos);
 showFullWindowImg(allPhotos);
 loadFormImg();
 validateForm();
 scaleImage();
 changeFilterEffect();
+
+
+
+
